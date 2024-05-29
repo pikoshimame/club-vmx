36a7ab1cdd5438de0514c29eb09aec62e938b81d
pikoshimame <pikoshimame@gmail.com>
2024-05-29 18:00:09 +0900

    update 2024-10 vmx site

--- src/components/Date/Date.tsx
+++ src/components/Date/Date.tsx
@@ -3,9 +3,9 @@ import { Wrapper, YearText, DateText, TimeText } from './styles'
 
 export function Date() {
   return (
-    <time css={Wrapper} dateTime="2023-10-15T14:00+09:00">
+    <time css={Wrapper} dateTime="2024-10-20T14:00+09:00">
       <span css={YearText}>2024</span>
-      <span css={DateText}>4/21</span>
+      <span css={DateText}>10/20</span>
       <span css={TimeText}>14:00-</span>
     </time>
   );
